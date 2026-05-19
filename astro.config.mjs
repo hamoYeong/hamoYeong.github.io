ee5a997889922bd204f3f916a2ee522401216345
yeong <rlatjdud6573@naver.com>
2026-05-19 20:44:44 +0900

    chore: GitHub Pages 사이트 주소 표기 정리

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hamoYeong.github.io',
+  site: 'https://hamoyeong.github.io',
 });
